fab577b13db571f1702305286360234e14821fcd
4sYnSw9p67 <IOd2H6xi6BDVV7rAwfWk@gmail.com>
2018-09-12 20:05:33 +0200

    updated to 2 bubbles

--- classes/bubble/bubble_init.js
+++ classes/bubble/bubble_init.js
@@ -2,16 +2,24 @@ let bubble;
 
 function setup() {
 	createCanvas(800, 600);
-	bubble = new Bubble(500, 300);
-	console.log(bubble.x, bubble.y);
+	bubble1 = new Bubble(500, 300);
+	bubble2 = new Bubble(200, 150);
+
+	console.log(bubble1.x, bubble1.y);
+	console.log(bubble2.x, bubble2.y);
 }
 
 function draw() {
 	background(0);
 
-	bubble.show();
-	bubble.move();
-	bubble.bounce();
+	bubble1.show();
+	bubble1.move();
+	bubble1.bounce();
+
+	bubble2.show();
+	bubble2.move();
+	bubble2.bounce();
+
 
 
 }
